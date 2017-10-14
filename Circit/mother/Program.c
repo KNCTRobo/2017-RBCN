326280c7c93139db55c6e2075a851bb958821a7d
robo-PC2\robo <knctrobo@gmail.com>
2017-10-14 18:47:09 +0900

    Aチームプログラムv1.02
    暴走問題の対処

--- Circit/mother/Program.c
+++ Circit/mother/Program.c
@@ -1,86 +1,118 @@
 /*	Program.c
  *	高専ロボコン2017 Aチームプログラム
- *	ver. 1.01
+ *	ver. 1.02
  */
 
 #include "includes.h"
 
 /* Funcs Predefine */
-void main(void);
-void setup(void);
-void motor_emit(char channnel, int power);
-void motor_reset(void);
-void PS2_read(void);
+/*	main()
+ *	プログラムのメインエントリポイントです */
+ void main(void);
+/*	setup()
+ *	プログラムのセットアップセッションです */
+ void setup(void);
+/*	motor_emit(char channnel,int power)
+ *	モータードライバにデータを送信します */
+ void motor_emit(char channnel, int power);
+/*	motor_reset()
+ *	モータードライバに出力0を送信します */
+ void motor_reset(void);
+/*	PS2_read()
+ *	コントローラからの信号を受信します
+ *	受信したデータはグローバル変数PS2_Dataに格納されます */
+ void PS2_read(void);
 /*	Data_Proc(int*)
- *	データ処理
- */	void Data_Proc(signed int* stat);
+ *	データ処理 */
+ void Data_Proc(signed int* stat);
 /*	Set_pwr(int*, int*, int)
- *	モータードライバ出力レベル決定
- */	void Set_pwr(signed int* stat, signed int* power);
-void motor_senddata(int* power, int* buffer, int index);
-int motor_isemit(int* power);
-void led_reset(void);
-void led_flash(void);
-void led_on(char pin);
-void led_off(char pin);
+ *	モータードライバ出力レベル決定 */
+ void Set_pwr(signed int* stat, signed int* power);
+/*	motor_senddata(int* power, int* buffer, int index)
+ *	モーターへの出力情報をまとめて送信します */
+ void motor_senddata(int* power, int* buffer, int index);
+/*	int = motor_isemit(int* power)
+ *	モーターへの出力状態を返します
+ *	1つ以上出力状態が0でない場合1を返します */
+ int motor_isemit(int* power);
+/*	led_reset()
+ *	LEDを全消灯します */
+ void led_reset(void);
+/*	led_flash()
+ *	LEDを点滅させます */
+ void led_flash(void);
+/*	led_reflesh(int LEDlogic_OPR, int LEDlogic_F1, int LEDlogic_F2, int LEDlogic_F3)
+ *	LEDの点灯状態を更新します */
+ void led_reflesh(int LEDlogic_OPR, int LEDlogic_F1, int LEDlogic_F2, int LEDlogic_F3);
+/*	led_on(char pin)
+ *	指定されたピンのLEDをONにします */
+ void led_on(char pin);
+/*	led_off(char pin)
+ *	指定されたピンのLEDをOFFにします */
+ void led_off(char pin);
 
 /* Global Vars */
 /*	uchar PS2_Data[PS2_DATA_BUFFER_SIZE]
- *	PSコントローラからのデータを格納するバッファ
- */	unsigned char PS2_Data[PS2_DATA_BUFFER_SIZE];
+ *	PSコントローラからのデータを格納するバッファ */
+ unsigned char PS2_Data[PS2_DATA_BUFFER_SIZE];
+/*	int PS2_offset
+ *コントローラのオフセット */
+ int PS2_offset = 0;
+/* 	const int pwt[]
+ *	移動モーターに出力するレベルのテーブル
+ *	内容はdefines.h内のPWR_MOV_TABLEで定義されています */
+ const int pwt[] = PWR_MOV_TABLE;
+/*	int rcv
+ *	コントローラの受信状態 */
+ int rcv = 0;
 #if ANALOG_ENABLE != 0
 /*	Analogs stick
- *	PSコントローラのアナログスティック情報
- */	Analogs Stick;
+ *	PSコントローラのアナログスティック情報 */
+ Analogs Stick;
 #endif
-/*	int PS2_offset
- *コントローラのオフセット
- */	int PS2_offset = 0;
-const int pwt[] = PWR_MOV_TABLE;
-/*	int rcv
- *	コントローラの受信状態
- */	int rcv = 0;
 
 void main()
 {
 	/* 変数宣言 */
 	/*	int i
-	 *	Index変数
-	 */	int i;
+	 *	Index変数 */
+	 int i;
 	/*	int* f
-	 *	Frame変数
-	 */	int f[2];
+	 *	Frame変数 */
+	 int f[2];
+	/*	long rcv_err
+	 *	PSコントローラからの通信が途絶えたときに通信断絶状態と判定するための変数
+	 *	閾値はRCV_THRESHOLDとする */
+	 long int rcv_err = RCV_THRESHOLD;
 	#if Program != 0
 	signed int level[MOTOR_NUM];
 	/*	int* pwr
-	 *	モータ出力レベル
-	 */	signed int pwr[MOTOR_NUM];
+	 *	モータ出力レベル */
+	 signed int pwr[MOTOR_NUM];
 	/*	int* motor_buf
-	 *	モータ出力レベル
-	 */	signed int motor_buf[MOTOR_NUM];
+	 *	モータ出力レベル */
+	 signed int motor_buf[MOTOR_NUM];
 	#endif
-	#if 0
-	/*	uchar motor_buf
-	 *	モータ出力状態バッファ
-	 */	unsigned char motor_buf = 0;
-	#endif
-	/*	long rcv_err
-	 *	PSコントローラからの通信が途絶えたときに通信断絶状態と判定するための変数
-	 *	閾値はRCV_THRESHOLDとする
-	 */	long int rcv_err = RCV_THRESHOLD;
-	/* 初期化 */
-	RESET:								/* ソフトリセット */
-	setup();							/* セットアップ */
-	motor_reset();						/* モータードライバに0(出力0%)を送信(暴走を防ぐため) */
-	/* LED試験点灯 */
+
+	/* 初期化
+	 * ソフトリセット時の復帰ポイント */
+	RESET:
+	/* セットアップ */
+	setup();
+	/* モータードライバに0(出力0%)を送信(暴走を防ぐため) */
+	motor_reset();
+
+	/* LED点滅 */
 	led_flash();
 	led_reset();
 	delay_ms(500);
+
 	/* メインループ */
 	for(;;)								/* 無限ループ */
 	{
 		/* 各変数をリフレッシュ */
 		#if Program != 0
+		/* 接続がタイムアウトしている場合ドライバ出力を0にする */
 		if (rcv == 0)
 		{
 			for(i = 0; i < MOTOR_NUM; i++)
@@ -92,8 +124,10 @@ void main()
 		#endif
 		f[0]++;	if (f[0] >= 2) f[0] = 0;
 		f[1]++;	if (f[1] >= MOTOR_NUM) f[1] = 0;
+
 		/* PSコントローラからデータ受信 */
 		PS2_read();
+
 		/* PSコントローラのデータ処理 */
 		if(PS2_Data[1 + PS2_offset]=='Z')
 		{
@@ -101,36 +135,37 @@ void main()
 			/* 再試行回数のカウントをリセット、通信状態のフラグを1にする */
 			rcv_err=0;
 			rcv = 1;
-			if(PS2_Data[2 + PS2_offset]&1) goto RESET;/* SELECTキーが押されたらソフトリセット */
+			/* SELECTキーが押されたらソフトリセット */
+			if(PS2_Data[2 + PS2_offset]&1) goto RESET;
+			/* ANALOGスティックが有効な場合スティック情報を取得 */
 			#if ANALOG_ENABLE != 0
 			Stick = gen_Analog(PS2_Data, PS2_offset);
 			#endif
+			/* データを処理し、ドライバ出力を決定 */
 			Data_Proc(level);
 			Set_pwr(level, pwr);
 		}
 		else
 		{
-		//PSコントローラから信号を受信できなかった時の処理
+			/* PSコントローラから信号を受信できなかった時の処理 */
 			if(rcv_err >= RCV_THRESHOLD)
 			{
-			//再試行回数が閾値を超えた場合通信断絶と判断する
+				/* 再試行回数が閾値を超えた場合通信断絶と判断する */
 				rcv = 0;
 			}
-			//再試行回数のカウント
-			else rcv_err++;
+			else
+			{
+				/* 再試行回数のカウント */
+				rcv_err++;
+			}
 		}
 		/* LED点灯制御 */
-		if(f[0])
-			led_on(LED_OPR);
-		else
-			led_off(LED_OPR);
-		if(EMITRULE_LED_F1)
-			led_on(LED_F1);
-		else
-			led_off(LED_F1);
+		led_reflesh(f[0], EMITRULE_LED_F1, EMITRULE_LED_F2, EMITRULE_LED_F3);
+
 		/* モータードライバの出力内容を決定、信号を出力する */
 		motor_senddata(pwr, motor_buf, f[1]);
 
+		/* ループ毎のディレイ */
 		#if LOOP_DELAYUNIT_US
 		delay_us(LOOP_DELAY);
 		#else
@@ -146,20 +181,23 @@ void setup(void)
 	set_tris_b(0xFF);
 	/*	portC入力ピン設定
 		C7は受信に使用するため該当ビットを1にする
-		XMIT=6,RCV=7
-	 */	set_tris_c(0x8D);
+		XMIT=6,RCV=7 */
+	set_tris_c(0x80);
 	/* オシレータの初期化 */
 	setup_oscillator(OSC_8MHZ);
 }
 
 void motor_emit(char channnel, int power)
 {
-	printf("%c%U*\r", channnel, power);
-	delay_us(TIME_MOTOR_MARGIN);
+	led_on(LED_F3);
+	printf("%c%U*\r", channnel, power);	/* データ送信 */
+	led_off(LED_F3);
+	delay_us(TIME_MOTOR_MARGIN);		/* データ送信後の待ち時間 */
 }
 
 void motor_reset(void)
 {
+	/* 全てのドライバに出力0を送信 */
 	printf("A%U*\r",0);   delay_us(TIME_MOTOR_MARGIN);
 	printf("B%U*\r",0);   delay_us(TIME_MOTOR_MARGIN);
 	printf("C%U*\r",0);   delay_us(TIME_MOTOR_MARGIN);
@@ -176,10 +214,11 @@ void PS2_read()
 	for(i = 0; i < PS2_DATA_BUFFER_SIZE; i++)
 		PS2_Data[i]=0;
 	PS2_offset = 0;
+	/* データを受信(データが来ない場合ここでプログラムが止まります) */
 	gets(PS2_Data);
 	for (i = 0; i < PS2_DATA_BUFFER_SIZE - 1; i++)
 	{
-		//PSコントローラの識別子0x5a('Z')を捜索、オフセットを決定
+		/* PSコントローラの識別子0x5a('Z')を捜索、オフセットを決定 */
 		if (PS2_Data[i] == 'Z')
 		{
 			PS2_offset = i - 1;
@@ -192,10 +231,13 @@ void Data_Proc(signed int* stat)
 /* ロボット1(釣り竿展開用ロボ) */
 #if (Program == 1)
 {
+	/* R1キーが押されている場合に処理を実行 */
 	if (PS2_PUSH_R1)
 	{
-		stat[0] = (PS2_PUSH_UP ? (PS2_PUSH_L1 ? 2 : 1) : 0) - (PS2_PUSH_DN ? (PS2_PUSH_L1 ? 2 : 1) : 0);
-		stat[1] = (PS2_PUSH_UP ? (PS2_PUSH_L1 ? 2 : 1) : 0) - (PS2_PUSH_DN ? (PS2_PUSH_L1 ? 2 : 1) : 0);
+		/* 前後移動 */
+		stat[0] = (PS2_PUSH_DN ? (PS2_PUSH_L1 ? 2 : 1) : 0) - (PS2_PUSH_UP ? (PS2_PUSH_L1 ? 2 : 1) : 0);
+		stat[1] = (PS2_PUSH_DN ? (PS2_PUSH_L1 ? 2 : 1) : 0) - (PS2_PUSH_UP ? (PS2_PUSH_L1 ? 2 : 1) : 0);
+		/* 左右旋回 */
 		if (PS2_PUSH_LE)
 		{
 			stat[0] += (PS2_PUSH_L1 ? 2 : 1);
@@ -206,6 +248,7 @@ void Data_Proc(signed int* stat)
 			stat[0] -= (PS2_PUSH_L1 ? 2 : 1);
 			stat[1] += (PS2_PUSH_L1 ? 2 : 1);
 		}
+		/* 配列のアウトレンジ回避 */
 		stat[0] = stat[0] > 2 ? 2 : stat[0];
 		stat[1] = stat[1] > 2 ? 2 : stat[1];
 		stat[0] = stat[0] < -2 ? -2 : stat[0];
@@ -217,56 +260,62 @@ void Data_Proc(signed int* stat)
 {
 	if (PS2_PUSH_R1)
 	{
-		stat[0]
-			= (PS2_STICK_LUP(Stick) ? (PS2_PUSH_L1 ? 2 : 1) : 0)
-			- (PS2_STICK_LDN(Stick) ? (PS2_PUSH_L1 ? 2 : 1) : 0);
-		stat[1]
-			= (PS2_STICK_LUP(Stick) ? (PS2_PUSH_L1 ? 2 : 1) : 0)
-			- (PS2_STICK_LDN(Stick) ? (PS2_PUSH_L1 ? 2 : 1) : 0);
-		stat[2]
-			= (PS2_STICK_LUP(Stick) ? (PS2_PUSH_L1 ? 2 : 1) : 0)
-			- (PS2_STICK_LDN(Stick) ? (PS2_PUSH_L1 ? 2 : 1) : 0);
-		stat[3]
-			= (PS2_STICK_LUP(Stick) ? (PS2_PUSH_L1 ? 2 : 1) : 0)
-			- (PS2_STICK_LDN(Stick) ? (PS2_PUSH_L1 ? 2 : 1) : 0);
+		/* 前後移動 */
+		stat[0] = (PS2_STICK_LUP(Stick) ? 1 : 0) - (PS2_STICK_LDN(Stick) ? 1 : 0);
+		stat[1] = (PS2_STICK_LUP(Stick) ? 1 : 0) - (PS2_STICK_LDN(Stick) ? 1 : 0);
+		stat[2] = (PS2_STICK_LUP(Stick) ? 1 : 0) - (PS2_STICK_LDN(Stick) ? 1 : 0);
+		stat[3] = (PS2_STICK_LUP(Stick) ? 1 : 0) - (PS2_STICK_LDN(Stick) ? 1 : 0);
+		/* 左右スライド移動 */
 		if (PS2_STICK_LRI(Stick))
 		{
-			stat[0] += (PS2_PUSH_L1 ? 2 : 1);
-			stat[1] -= (PS2_PUSH_L1 ? 2 : 1);
-			stat[2] -= (PS2_PUSH_L1 ? 2 : 1);
-			stat[3] += (PS2_PUSH_L1 ? 2 : 1);
+			stat[0] += 1;
+			stat[1] -= 1;
+			stat[2] -= 1;
+			stat[3] += 1;
 		}
 		if (PS2_STICK_LLE(Stick))
 		{
-			stat[0] -= (PS2_PUSH_L1 ? 2 : 1);
-			stat[1] += (PS2_PUSH_L1 ? 2 : 1);
-			stat[2] += (PS2_PUSH_L1 ? 2 : 1);
-			stat[3] -= (PS2_PUSH_L1 ? 2 : 1);
+			stat[0] -= 1;
+			stat[1] += 1;
+			stat[2] += 1;
+			stat[3] -= 1;
 		}
+		/* 左右旋回 */
 		if (PS2_PUSH_LE)
 		{
-			stat[0] = -(PS2_PUSH_L1 ? 2 : 1);
-			stat[1] = (PS2_PUSH_L1 ? 2 : 1);
-			stat[2] = -(PS2_PUSH_L1 ? 2 : 1);
-			stat[3] = (PS2_PUSH_L1 ? 2 : 1);
+			stat[0] = -1;
+			stat[1] = 1;
+			stat[2] = -1;
+			stat[3] = 1;
 		}
 		if (PS2_PUSH_RI)
 		{
-			stat[0] = (PS2_PUSH_L1 ? 2 : 1);
-			stat[1] = -(PS2_PUSH_L1 ? 2 : 1);
-			stat[2] = (PS2_PUSH_L1 ? 2 : 1);
-			stat[3] = -(PS2_PUSH_L1 ? 2 : 1);
+			stat[0] = 1;
+			stat[1] = -1;
+			stat[2] = 1;
+			stat[3] = -1;
 		}
-		stat[0] = stat[0]>2 ? 2 : stat[0];
-		stat[1] = stat[1]>2 ? 2 : stat[1];
-		stat[2] = stat[2]>2 ? 2 : stat[2];
-		stat[3] = stat[3]>2 ? 2 : stat[3];
-		stat[0] = stat[0]<-2 ? -2 : stat[0];
-		stat[1] = stat[1]<-2 ? -2 : stat[1];
-		stat[2] = stat[2]<-2 ? -2 : stat[2];
-		stat[3] = stat[3]<-2 ? -2 : stat[3];
+		/* 配列のアウトレンジ回避 */
+		stat[0] = stat[0]>1 ? 1 : stat[0];
+		stat[1] = stat[1]>1 ? 1 : stat[1];
+		stat[2] = stat[2]>1 ? 1 : stat[2];
+		stat[3] = stat[3]>1 ? 1 : stat[3];
+		stat[0] = stat[0]<-1 ? -1 : stat[0];
+		stat[1] = stat[1]<-1 ? -1 : stat[1];
+		stat[2] = stat[2]<-1 ? -1 : stat[2];
+		stat[3] = stat[3]<-1 ? -1 : stat[3];
+		/* L1での加速処理 */
+		if (PS2_PUSH_L1)
+		{
+			stat[0] *= 2;
+			stat[1] *= 2;
+			stat[2] *= 2;
+			stat[3] *= 2;
+		}
 	}
 }
+#else
+{}
 #endif
 
 void Set_pwr(signed int* stat, signed int* power)
@@ -293,6 +342,8 @@ void Set_pwr(signed int* stat, signed int* power)
 	if (PWR_MOVEC_INVERT) power[2] = -power[2];
 	if (PWR_MOVED_INVERT) power[3] = -power[3];
 }
+#else
+{}
 #endif
 
 void motor_senddata(int* power, int* buffer, int index)
@@ -393,6 +444,8 @@ void motor_senddata(int* power, int* buffer, int index)
 		buffer[5] = power[5];
 	}
 }
+#else
+{}
 #endif
 
 int motor_isemit(int* power)
@@ -409,6 +462,8 @@ void led_reset(void)
 {
 	led_off(LED_OPR);
 	led_off(LED_F1);
+	led_off(LED_F2);
+	led_off(LED_F3);
 }
 
 void led_flash(void)
@@ -417,10 +472,34 @@ void led_flash(void)
 	int i;
 	for(i= 5; i; i--){
 		led_on(LED_F1);
+		led_on(LED_F2);
+		led_on(LED_F3);
 		delay_ms(200);
 		led_off(LED_F1);
+		led_off(LED_F2);
+		led_off(LED_F3);
 		delay_ms(200);
 	}
+}
+
+void led_reflesh(int LEDlogic_OPR, int LEDlogic_F1, int LEDlogic_F2, int LEDlogic_F3)
+{
+	if (LEDlogic_OPR)
+		led_on(LED_OPR);
+	else
+		led_off(LED_OPR);
+	if (LEDlogic_F1)
+		led_on(LED_F1);
+	else
+		led_off(LED_F1);
+	if (LEDlogic_F2)
+		led_on(LED_F2);
+	else
+		led_off(LED_F2);
+	if (LEDlogic_F3)
+		led_on(LED_F3);
+	else
+		led_off(LED_F3);
 }
 
 void led_on(char pin)
--- Circit/mother/defines.h
+++ Circit/mother/defines.h
@@ -7,17 +7,21 @@
 #include <16F886.h>
 
 #fuses INTRC_IO,WDT,NOPROTECT,PUT,NOMCLR,NOLVP,BROWNOUT
-#use delay(CLOCK=8000000)
 #use fast_io(a)//先に宣言する。
 #use fast_io(b)
 #use fast_io(c)
 #use fast_io(e)
+/*	delay使用
+ *	オシレータクロック	8MHz
+ */
+ #use delay(CLOCK=8000000)
 /*	RS232(USART)モジュール使用
  *	速度			9600Hz
  *	受信ピン		C7
  *	送信ピン		C6
  *	エラー記録	有効
- */	#use RS232(BAUD=9600,RCV=PIN_C7,XMIT=PIN_C6,ERRORS,FORCE_SW)
+ */
+ #use RS232(BAUD=9600,RCV=PIN_C7,XMIT=PIN_C6,ERRORS,FORCE_SW)
 #byte port_a = 5
 #byte port_b = 6
 #byte port_c = 7
@@ -26,65 +30,99 @@
 
 /*	Program
  *	コンパイル制御による制御プログラムの選択
- */	#define Program	0
+ */	#define Program	2
 
 /* 共通設定項目 */
 	/*	F_TIME
 	 *	LED試験点灯のLED点灯時間
-	 */	#define F_TIME	200
+	 */
+	 #define F_TIME	200
 	/*	LED_PULLUP
-	 *	LED1のプルアップ使用フラグ
-	 */	#define LED_PULLUP			   1
+	 *	LEDのプルアップ使用フラグ
+	 */
+	 #define LED_PULLUP			   0
 	/*	LED_OPR
 	 *	オペレーションLEDのピン設定
-	 */	#define LED_OPR				PIN_A0
+	 */
+	 #define LED_OPR				PIN_C5
 	/*	LED_F1
 	 *	LED1のピン設定
-	 */	#define LED_F1				PIN_A2
+	 */
+	 #define LED_F1				PIN_C0
+	/*	LED_F2
+	 *	LED1のピン設定
+	 */
+	 #define LED_F2				PIN_C1
+	/*	LED_F3
+	 *	LED1のピン設定
+	 */
+	 #define LED_F3				PIN_C2
 	/*	EMITRULE_LED_F1
 	 *	LED1の点灯条件
-	 */	#define EMITRULE_LED_F1		((rcv && !PS2_PUSH_R1) || motor_isemit(pwr))
+	 */
+	 #define EMITRULE_LED_F1		((rcv && !PS2_PUSH_R1))
+	/*	EMITRULE_LED_F2
+	 *	LED1の点灯条件
+	 */
+	 #define EMITRULE_LED_F2		(motor_isemit(pwr))
+	/*	EMITRULE_LED_F3
+	 *	LED1の点灯条件
+	 */
+	 #define EMITRULE_LED_F3		(0)
 #if Program == 1
 /* ロボット1"鶴(サロルン)" 設定項目 */
 	/*	MOTOR_NUM
 	 *	使用モーター数
-	 */	#define MOTOR_NUM	5
+	 */
+	 #define MOTOR_NUM	5
 	/*	MOTOR_MOVER
 	 *	タイヤ右
-	 */	#define MOTOR_MOVER 'R'
+	 */
+	 #define MOTOR_MOVER 'R'
 	/*	MOTOR_MOVEL
 	 *	タイヤ左
-	 */	#define MOTOR_MOVEL 'L'
+	 */
+	 #define MOTOR_MOVEL 'L'
 	/*	MOTOR_AIRa
 	 *	エアー1
-	 */	#define MOTOR_AIRa 'A'
+	 */
+	 #define MOTOR_AIRa 'A'
 	/*	MOTOR_AIRb
 	 *	エアー2
-	 */	#define MOTOR_AIRb 'B'
+	 */
+	 #define MOTOR_AIRb 'B'
 	/*	MOTOR_ARM
 	 *	竿
-	 */	#define MOTOR_ARM 'C'
+	 */
+	 #define MOTOR_ARM 'C'
 	/*	PWR_MOV_TABLE
 	 *	移動に用いるパワー
-	 */	#define PWR_MOV_TABLE	{-100, -30, 0, 30, 100}
+	 */
+	 #define PWR_MOV_TABLE	{-100, -30, 0, 30, 100}
 	/*	PWR_AIR
 	 *	エアシリンダ駆動に用いるパワー
-	 */	#define PWR_AIR	100
+	 */
+	 #define PWR_AIR	100
 	/*	PWR_ARM
 	 *	アーム駆動に用いるパワー
-	 */	#define PWR_ARM	100
+	 */
+	 #define PWR_ARM	100
 	/*	LOOP_DELAY
 	 *	プログラムループの待ち時間
-	 */	#define LOOP_DELAY	0
+	 */
+	 #define LOOP_DELAY	0
 	/*	LOOP_DELAYUNIT_US
 	 *	プログラムループの待ち時間の単位をusにするかどうか
-	 */	#define LOOP_DELAYUNIT_US	1
+	 */
+	 #define LOOP_DELAYUNIT_US	1
 	/*	TIME_MOTOR_MARGIN
 	 *	モータードライバに信号を送信した直後の待ち時間
-	 */	#define TIME_MOTOR_MARGIN	450
+	 */
+	 #define TIME_MOTOR_MARGIN	450
 	/*	DATA_BUFFER_SIZE
 	 *	受信データのバッファサイズ
-	 */	#define PS2_DATA_BUFFER_SIZE		  32
+	 */
+	 #define PS2_DATA_BUFFER_SIZE		  32
 	/*	RCV_THRESHOLD
 	 *	PS2の信号が途絶えた際に通信断絶状態と判定するための閾値
 	 */	#define RCV_THRESHOLD		10
@@ -92,81 +130,77 @@
 	#define ANALOG_THRESHOLD	80
 #elif Program == 2
 /* ロボット2"スマリン" 設定項目 */
-	#define MOTOR_NUM	6
+	/*	MOTOR_NUM
+	 *	使用モーター数 */
+	 #define MOTOR_NUM	6
 	/*	MOTOR_MOVEA
-	 *	メカナム左前
-	 */	#define MOTOR_MOVEA	'A'
+	 *	メカナム左前 */
+	 #define MOTOR_MOVEA	'A'
 	/*	MOTOR_MOVEB
-	 *	メカナム右前
-	 */	#define MOTOR_MOVEB	'B'
+	 *	メカナム右前 */
+	 #define MOTOR_MOVEB	'B'
 	/*	MOTOR_MOVEC
-	 *	メカナム左後
-	 */	#define MOTOR_MOVEC	'C'
+	 *	メカナム左後 */
+	 #define MOTOR_MOVEC	'C'
 	/*	MOTOR_MOVED
-	 *	メカナム右後
-	 */	#define MOTOR_MOVED	'D'
+	 *	メカナム右後 */
+	 #define MOTOR_MOVED	'D'
 	/*	MOTOR_AIR
-	 *	エアー
-	 */	#define MOTOR_AIR	'E'
+	 *	エアー */
+	 #define MOTOR_AIR	'E'
 	/*	MOTOR_ARM
-	 *	竿
-	 */	#define MOTOR_ARM	'F'
+	 *	竿 */
+	 #define MOTOR_ARM	'F'
 	/*	PWR_MOV_TABLE
-	 *	移動に用いるパワー
-	 */	#define PWR_MOV_TABLE	{-100, -30, 0, 30, 100}
+	 *	移動に用いるパワー */
+	 #define PWR_MOV_TABLE	{-100, -30, 0, 30, 100}
 	/*	PWR_AIR
-	 *	エアシリンダ駆動に用いるパワー
-	 */	#define PWR_AIR	100
+	 *	エアシリンダ駆動に用いるパワー */
+	 #define PWR_AIR	100
 	/*	PWR_ARM
-	 *	アーム駆動に用いるパワー
-	 */	#define PWR_ARM	100
-	/*	PWR_MOVEA_INVERT
-	 *	駆動モータA符号反転フラグ
-	 */	#define PWR_MOVEA_INVERT 0
-	/*	PWR_MOVEB_INVERT
-	 *	駆動モータA符号反転フラグ
-	 */	#define PWR_MOVEB_INVERT 0
-	/*	PWR_MOVEC_INVERT
-	 *	駆動モータA符号反転フラグ
-	 */	#define PWR_MOVEC_INVERT 0
-	/*	PWR_MOVED_INVERT
-	 *	駆動モータA符号反転フラグ
-	 */	#define PWR_MOVED_INVERT 1
+	 *	アーム駆動に用いるパワー */
+	 #define PWR_ARM	100
+	/*	PWR_INVERT
+	 *	モータ符号反転フラグ */
+	 #define PWR_MOVEA_INVERT 0
+	 #define PWR_MOVEB_INVERT 0
+	 #define PWR_MOVEC_INVERT 0
+	 #define PWR_MOVED_INVERT 1
 	/*	LOOP_DELAY
-	 *	プログラムループの待ち時間
-	 */	#define LOOP_DELAY	0
+	 *	プログラムループの待ち時間 */
+	 #define LOOP_DELAY	0
 	/*	LOOP_DELAYUNIT_US
-	 *	プログラムループの待ち時間の単位をusにするかどうか
-	 */	#define LOOP_DELAYUNIT_US	1
+	 *	プログラムループの待ち時間の単位をusにするかどうか */
+	 #define LOOP_DELAYUNIT_US	1
 	/*	TIME_MOTOR_MARGIN
-	 *	モータードライバに信号を送信した直後の待ち時間
-	 */	#define TIME_MOTOR_MARGIN	2000
+	 *	モータードライバに信号を送信した直後の待ち時間 */
+	 #define TIME_MOTOR_MARGIN	2000
 	/*	DATA_BUFFER_SIZE
-	 *	受信データのバッファサイズ
-	 */	#define PS2_DATA_BUFFER_SIZE		  32
+	 *	受信データのバッファサイズ */
+	 #define PS2_DATA_BUFFER_SIZE		  32
 	/*	RCV_THRESHOLD
-	 *	PS2の信号が途絶えた際に通信断絶状態と判定するための閾値
-	 */	#define RCV_THRESHOLD		   3
+	 *	PS2の信号が途絶えた際に通信断絶状態と判定するための閾値 */
+	 #define RCV_THRESHOLD		   3
 	#define ANALOG_ENABLE	1
 	#define ANALOG_THRESHOLD	80
 #else
 /* デフォルト値 */
 	#define PWR_MOV_TABLE	{0}
 	/*	LOOP_DELAY
-	 *	プログラムループの待ち時間
-	 */	#define LOOP_DELAY	0
+	 *	プログラムループの待ち時間 */
+	 #define LOOP_DELAY	0
 	/*	LOOP_DELAYUNIT_US
-	 *	プログラムループの待ち時間の単位をusにするかどうか
-	 */	#define LOOP_DELAYUNIT_US	1
+	 *	プログラムループの待ち時間の単位をusにするかどうか */
+	 #define LOOP_DELAYUNIT_US	1
 	/*	TIME_MOTOR_MARGIN
-	 *	モータードライバに信号を送信した直後の待ち時間
-	 */	#define TIME_MOTOR_MARGIN	0
+	 *	モータードライバに信号を送信した直後の待ち時間 */
+	 #define TIME_MOTOR_MARGIN	0
 	/*	DATA_BUFFER_SIZE
-	 *	受信データのバッファサイズ
-	 */	#define PS2_DATA_BUFFER_SIZE		  32
+	 *	受信データのバッファサイズ */
+	 #define PS2_DATA_BUFFER_SIZE		  32
 	/*	RCV_THRESHOLD
-	 *	PS2の信号が途絶えた際に通信断絶状態と判定するための閾値
-	 */	#define RCV_THRESHOLD		   3
+	 *	PS2の信号が途絶えた際に通信断絶状態と判定するための閾値 */
+	 #define RCV_THRESHOLD		   3
 	#define ANALOG_ENABLE	0
 	#define ANALOG_THRESHOLD	80
 
